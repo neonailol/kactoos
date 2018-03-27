97278eb2a0f84e7b3f676995b39b5221fa6b8b53
Valeriy Zhirnov <neonailol@gmail.com>
2018-03-27 11:22:33 +0300

    Constant Test

---
+++ kactoos-common/src/test/kotlin/nnl/rocks/kactoos/scalar/ConstantTest.kt
@@ -0,0 +1,28 @@
+package nnl.rocks.kactoos.scalar
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertSame
+
+class ConstantTest {
+
+    @Test
+    fun returnsGivenValue() {
+        val value = "Hello, World!"
+        assertEquals(
+            expected = value,
+            actual = Constant(value).value(),
+            message = "Returned value differ from expected"
+        )
+    }
+
+    @Test
+    fun alwaysReturnsSameValue() {
+        val value = "Hello, World!"
+        assertSame(
+            expected = value,
+            actual = Constant(value).value(),
+            message = "Returned value not same as expected"
+        )
+    }
+}
