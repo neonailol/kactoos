0fb14d9a5ff1e82a92218f9c9c89c92f2a509452
Neonailol <neonailol@gmail.com>
2018-03-28 15:47:13 +0300

    Migrate javascript project to kotlin-dsl

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,7 +1,7 @@
-configurations.compile.dependencies.clear()
+//configurations.compile.dependencies.clear()
 
 plugins {
     kotlin("jvm") version "1.2.31"
-    `embedded-kotlin` version "0.16.2"
-    `kotlin-dsl` version "0.16.2"
+//    `embedded-kotlin` version "0.16.2"
+//    `kotlin-dsl` version "0.16.2"
 }
