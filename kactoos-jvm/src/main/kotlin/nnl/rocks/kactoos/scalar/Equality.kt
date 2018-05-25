62208900a9d4c12cbd5b8c9fc87717894c84488e
Neonailol <neonailol@gmail.com>
2018-05-25 15:49:18 +0300

    port Equality

---
+++ kactoos-jvm/src/main/kotlin/nnl/rocks/kactoos/scalar/Equality.kt
@@ -0,0 +1,45 @@
+package nnl.rocks.kactoos.scalar
+
+import nnl.rocks.kactoos.Bytes
+import nnl.rocks.kactoos.Scalar
+import kotlin.math.sign
+
+/**
+ * Equality.
+ *
+ * Returns:
+ * * the value `0` if `x == y`;
+ * * the value `-1` if `x < y`;
+ * * the value `1` if `x > y`
+ *
+ * There is no thread-safety guarantee.
+ *
+ * @param T Type of input
+ * @param left Left
+ * @param right Right
+ * @since 0.5
+ */
+class Equality<T : Bytes>(
+    private val left: T,
+    private val right: T
+) : Scalar<Int> {
+
+    override fun invoke(): Int {
+        val lft = left.asBytes()
+        val rght = right.asBytes()
+        return Ternary<Int, Any>(
+            { lft.size == rght.size },
+            {
+                var result = 0
+                for (idx in rght.indices.reversed()) {
+                    result = lft[idx] - rght[idx]
+                    if (result != 0) {
+                        break
+                    }
+                }
+                result.sign
+            },
+            { (lft.size - rght.size).sign }
+        ).invoke()
+    }
+}
