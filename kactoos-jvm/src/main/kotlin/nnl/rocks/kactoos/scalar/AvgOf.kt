c54f2d0db33b89fdadee5e83f7624a1158fa4b6b
Neonailol <neonailol@gmail.com>
2018-05-26 13:14:04 +0300

    Tests for AvgOf

--- kactoos-jvm/src/main/kotlin/nnl/rocks/kactoos/scalar/AvgOf.kt
+++ kactoos-jvm/src/main/kotlin/nnl/rocks/kactoos/scalar/AvgOf.kt
@@ -1,9 +1,8 @@
 package nnl.rocks.kactoos.scalar
 
 import nnl.rocks.kactoos.KScalar
-import nnl.rocks.kactoos.func.BiFuncOf
+import nnl.rocks.kactoos.Scalar
 import nnl.rocks.kactoos.func.FuncOf
-import nnl.rocks.kactoos.iterable.IterableOf
 import nnl.rocks.kactoos.iterable.LengthOf
 import nnl.rocks.kactoos.iterable.Mapped
 import java.math.BigDecimal
@@ -14,88 +13,59 @@ import java.math.MathContext
  *
  * Here is how you can use it to fine mathematical average of numbers:
  *
- * ```java
- * int sum = new AvgOf(1, 2, 3, 4).intValue();
- * long sum = new AvgOf(1L, 2L, 3L).longValue();
- * int sum = new AvgOf(numbers.toArray(new Integer[numbers.size()])).intValue();
+ * ```kotlin
+ * val avg = AvgOf(1, 2, 3).toInt()
+ * val avg = AvgOf(1L, 2L, 3L).toLong()
  * ```
  *
  * There is no thread-safety guarantee.
  *
  * @param src The iterable
- * @since 0.24
+ * @since 0.5
  */
-class AvgOf(src: Iterable<KScalar<Number>>) : NumberEnvelope(Ternary(
-    LengthOf(src).toLong(),
-    FuncOf { len -> len > 0 },
-    FuncOf { len ->
-        Folded<BigDecimal, BigDecimal>(
-            BigDecimal.ZERO,
-            BiFuncOf { sum, value -> sum.add(value, MathContext.DECIMAL128) },
-            Mapped<KScalar<Number>, BigDecimal>(
-                FuncOf { number ->
-                    BigDecimal.valueOf(
-                        number().toDouble()
-                    )
-                },
-                src
-            )
-        ).invoke().divide(
-            BigDecimal.valueOf(len),
-            MathContext.DECIMAL128
-        ).toDouble()
-    },
-    FuncOf { 0.0 }
-)) {
+class AvgOf(src: Iterable<KScalar<Number>>) : NumberEnvelope(
+    Ternary(
+        LengthOf(src).toLong(),
+        FuncOf { len -> len > 0 },
+        FuncOf { len ->
+            src.map { BigDecimal.valueOf(it().toDouble()) }
+                .fold(BigDecimal.ZERO) { acc, r -> acc.add(r, MathContext.DECIMAL128) }
+                .divide(BigDecimal.valueOf(len), MathContext.DECIMAL128)
+                .toDouble()
+        },
+        FuncOf { 0.0 }
+    )
+) {
 
-    /**
-     * Ctor.
-     * @param src Numbers
-     */
     constructor(vararg src: Int) : this(
-        Mapped<Int, KScalar<Number>>(
-            FuncOf<Int, KScalar<Number>> { number ->  { number } },
+        Mapped(
+            FuncOf { number: Int -> { number } },
             Iterable { src.iterator() }
         )
     )
 
-    /**
-     * Ctor.
-     * @param src Numbers
-     */
     constructor(vararg src: Long) : this(
-        Mapped<Long, KScalar<Number>>(
-            FuncOf { number ->  { number } },
+        Mapped(
+            FuncOf { number: Long -> { number } },
             Iterable { src.iterator() }
         )
     )
 
-    /**
-     * Ctor.
-     * @param src Numbers
-     */
     constructor(vararg src: Double) : this(
-        Mapped<Double, KScalar<Number>>(
-            FuncOf { number ->  { number } },
+        Mapped(
+            FuncOf { number: Double -> { number } },
             Iterable { src.iterator() }
         )
     )
 
-    /**
-     * Ctor.
-     * @param src Numbers
-     */
     constructor(vararg src: Float) : this(
-        Mapped<Float, KScalar<Number>>(
-            FuncOf { number ->  { number } },
+        Mapped(
+            FuncOf { number: Float -> { number } },
             Iterable { src.iterator() }
         )
     )
 
-    /**
-     * Ctor.
-     * @param src Numbers
-     */
-    @SafeVarargs
-    constructor(vararg src: KScalar<Number>) : this(IterableOf<KScalar<Number>>(*src))
+    constructor(vararg src: KScalar<Number>) : this(Iterable { src.iterator() })
+
+    constructor(vararg src: Scalar<Number>) : this(Iterable { src.map { { it() } }.iterator() })
 }
---
+++ kactoos-jvm/src/test/kotlin/nnl/rocks/kactoos/scalar/AvgOfTest.kt
@@ -0,0 +1,341 @@
+package nnl.rocks.kactoos.scalar
+
+import nnl.rocks.kactoos.Scalar
+import nnl.rocks.kactoos.iterable.IterableOf
+import org.testng.Assert.assertEquals
+import org.testng.annotations.Test
+
+class AvgOfTest {
+
+    @Test
+    fun withEmptyCollection() {
+        assertEquals(
+            AvgOf(emptyList()).toLong(),
+            0L
+        )
+    }
+
+    @Test
+    fun withIntCollectionIntValue() {
+        assertEquals(
+            AvgOf(
+                1, 2, 3, 4
+            ).toInt(),
+            2
+        )
+    }
+
+    @Test
+    fun withIntCollectionIntValueMaxValues() {
+        assertEquals(
+            AvgOf(
+                Integer.MAX_VALUE, Integer.MAX_VALUE
+            ).toInt(),
+            Integer.MAX_VALUE
+        )
+    }
+
+    @Test
+    fun withIntCollectionLongValue() {
+        assertEquals(
+            AvgOf(
+                1, 2, 3, 4
+            ).toLong(),
+            2L
+        )
+    }
+
+    @Test
+    fun withIntCollectionDoubleValue() {
+        assertEquals(
+            AvgOf(
+                1, 2, 3, 4
+            ).toDouble(),
+            2.5
+        )
+    }
+
+    @Test
+    fun withIntCollectionFloatValue() {
+        assertEquals(
+            AvgOf(
+                1, 2, 3, 4
+            ).toFloat(),
+            2.5f
+        )
+    }
+
+    @Test
+    fun withLongCollectionIntValue() {
+        assertEquals(
+            AvgOf(
+                1L, 2L, 3L, 4L
+            ).toInt(),
+            2
+        )
+    }
+
+    @Test
+    fun withLongCollectionLongValue() {
+        assertEquals(
+            AvgOf(
+                1L, 2L, 3L, 4L
+            ).toLong(),
+            2L
+        )
+    }
+
+    @Test
+    fun withLongCollectionMaxValue() {
+        assertEquals(
+            AvgOf(
+                Long.MAX_VALUE, Long.MAX_VALUE
+            ).toLong(),
+            Long.MAX_VALUE
+        )
+    }
+
+    @Test
+    fun withLongCollectionDoubleValue() {
+        assertEquals(
+            AvgOf(
+                1L, 2L, 3L, 4L
+            ).toDouble(),
+            2.5
+        )
+    }
+
+    @Test
+    fun withLongCollectionFloatValue() {
+        assertEquals(
+            AvgOf(
+                1L, 2L, 3L, 4L
+            ).toFloat(),
+            2.5f
+        )
+    }
+
+    @Test
+    fun withDoubleCollectionIntValue() {
+        assertEquals(
+            AvgOf(
+                1.0, 2.0, 3.0, 4.0
+            ).toInt(),
+            2
+        )
+    }
+
+    @Test
+    fun withDoubleCollectionLongValue() {
+        assertEquals(
+            AvgOf(
+                1.0, 2.0, 3.0, 4.0
+            ).toLong(),
+            2L
+        )
+    }
+
+    @Test
+    fun withDoubleCollectionDoubleValue() {
+        assertEquals(
+            AvgOf(
+                1.0, 2.0, 3.0, 4.0
+            ).toDouble(),
+            2.5
+        )
+    }
+
+    @Test
+    fun withDoubleCollectionMaxValue() {
+        assertEquals(
+            AvgOf(
+                Double.MAX_VALUE, Double.MAX_VALUE
+            ).toDouble(),
+            Double.MAX_VALUE
+        )
+    }
+
+    @Test
+    fun withDoubleCollectionMinValue() {
+        assertEquals(
+            AvgOf(
+                Double.MIN_VALUE, Double.MIN_VALUE
+            ).toDouble(),
+            Double.MIN_VALUE
+        )
+    }
+
+    @Test(expectedExceptions = [NumberFormatException::class])
+    fun withDoubleCollectionPositiveInfinity() {
+        AvgOf(
+            Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY
+        ).toDouble()
+    }
+
+    @Test(expectedExceptions = [NumberFormatException::class])
+    fun withDoubleCollectionNegativeInfinity() {
+        AvgOf(
+            Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY
+        ).toDouble()
+    }
+
+    @Test(expectedExceptions = [NumberFormatException::class])
+    fun withDoubleCollectionNaN() {
+        AvgOf(
+            Double.NaN, Double.NaN
+        ).toDouble()
+    }
+
+    @Test
+    fun withDoubleCollectionNegativeNumbersDoubleValue() {
+        assertEquals(
+            AvgOf(
+                - 1.0, - 2.0, - 3.0, - 4.0
+            ).toDouble(),
+            - 2.5
+        )
+    }
+
+    @Test
+    fun withDecimalCollectionPrecisionProblem() {
+        assertEquals(
+            AvgOf(
+                100.0, 100.666, 100.0
+            ).toFloat(),
+            100.222f
+        )
+    }
+
+    @Test
+    fun withDecimalCollectionPrecisionProblemExtraDecimalRange() {
+        assertEquals(
+            AvgOf(
+                100.266, 100.267
+            ).toFloat(),
+            100.2665f
+        )
+    }
+
+    @Test
+    fun withFloatCollectionIntValue() {
+        assertEquals(
+            AvgOf(
+                1.0f, 2.0f, 3.0f, 4.0f
+            ).toInt(),
+            2
+        )
+    }
+
+    @Test
+    fun withFloatCollectionLongValue() {
+        assertEquals(
+            AvgOf(
+                1.0f, 2.0f, 3.0f, 4.0f
+            ).toLong(),
+            2L
+        )
+    }
+
+    @Test
+    fun withFloatCollectionDoubleValue() {
+        assertEquals(
+            AvgOf(
+                1.0f, 2.0f, 3.0f, 4.0f
+            ).toDouble(),
+            2.5
+        )
+    }
+
+    @Test
+    fun withFloatCollectionFloatValue() {
+        assertEquals(
+            AvgOf(
+                1.0f, 2.0f, 3.0f, 4.0f
+            ).toFloat(),
+            2.5f
+        )
+    }
+
+    @Test
+    fun withFloatCollectionMaxValue() {
+        assertEquals(
+            AvgOf(
+                Float.MAX_VALUE, Float.MAX_VALUE
+            ).toFloat(),
+            Float.MAX_VALUE
+        )
+    }
+
+    @Test
+    fun withFloatCollectionMinValue() {
+        assertEquals(
+            AvgOf(
+                Float.MIN_VALUE, Float.MIN_VALUE
+            ).toFloat(),
+            Float.MIN_VALUE
+        )
+    }
+
+    @Test
+    fun withIntScalarsIntValue() {
+        assertEquals(
+            AvgOf(
+                { 1 }, { 2 }, { 10 }
+            ).toInt(),
+            4
+        )
+    }
+
+    @Test
+    fun withLongScalarsIntValue() {
+        assertEquals(
+            AvgOf(
+                { 1L }, { 2L }, { 10L }
+            ).toInt(),
+            4
+        )
+    }
+
+    @Test
+    fun withFloatScalarsIntValue() {
+        assertEquals(
+            AvgOf(
+                { 1.0f }, { 2.0f }, { 10.0f }
+            ).toLong(),
+            4L
+        )
+    }
+
+    @Test
+    fun withDoubleScalarsIntValue() {
+        assertEquals(
+            AvgOf(
+                { 1.0 }, { 2.0 }, { 10.0 }
+            ).toInt(),
+            4
+        )
+    }
+
+    @Test
+    fun withIterableOfScalars() {
+        assertEquals(
+            AvgOf(
+                IterableOf({ 1L }, { 2L }, { 10L })
+            ).toLong(),
+            4L
+        )
+    }
+
+    @Test
+    fun withCompositionOfScalars() {
+        assertEquals(
+            AvgOf(
+                { MinOf(1.0, 2.0).invoke() },
+                { MaxOf(2.0, 4.0).invoke() },
+                { SumOf(1.0, 2.0, 2.0).invoke() },
+                { Ternary<Scalar<Number>, Scalar<Number>>(true, Constant { 5.0 }, Constant { 1.0 }).invoke().invoke() }
+            ).toInt(),
+            3
+        )
+    }
+}
