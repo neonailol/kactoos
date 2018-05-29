5e3a084983d94936ec81fda08200236f6ed79ad8
Neonailol <neonailol@gmail.com>
2018-05-29 18:57:54 +0300

    Grouped tests

--- kactoos-common/src/test/kotlin/nnl/rocks/kactoos/map/GroupedTest.kt
+++ kactoos-common/src/test/kotlin/nnl/rocks/kactoos/map/GroupedTest.kt
@@ -6,12 +6,22 @@ import kotlin.test.assertTrue
 class GroupedTest {
 
     @Test
-    fun groups() {
+    fun groupsByChar() {
         val grouped = Grouped(
             listOf('a', 'b', 'c'),
             { c: Char -> c.toUpperCase() },
-            { x: Char -> x.toString() }
+            { c: Char -> c.toString() }
         )
         assertTrue(grouped['A']?.containsAll(listOf("a")) ?: false)
+    }
+
+    @Test
+    fun groupsByIntegers() {
+        val grouped = Grouped(
+            listOf(2, 2, 3, 4, 5),
+            { i: Int -> i },
+            { i: Int -> i.toString() }
+        )
+        assertTrue(grouped[2]?.containsAll(listOf("2", "2")) ?: false)
     }
 }
