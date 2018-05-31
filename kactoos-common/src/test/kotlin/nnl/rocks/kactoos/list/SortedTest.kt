eca26734609fcb4f51e8eec0a05508cd9dae59a5
Neonailol <neonailol@gmail.com>
2018-05-31 17:05:07 +0300

    Tests for sorted list

---
+++ kactoos-common/src/test/kotlin/nnl/rocks/kactoos/list/SortedTest.kt
@@ -0,0 +1,27 @@
+package nnl.rocks.kactoos.list
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SortedTest {
+
+    @Test
+    fun sortsNumbers() {
+        val sorted = Sorted(7, 6, 5)
+        assertEquals(5, sorted[0])
+        assertEquals(6, sorted[1])
+        assertEquals(7, sorted[2])
+    }
+
+    @Test
+    fun sortsUsingComparator() {
+        val sorted = Sorted(
+            Comparator { a, b -> a.first().compareTo(b.first()) },
+            listOf("the", "red", "fox")
+        )
+        assertEquals("fox", sorted[0])
+        assertEquals("red", sorted[1])
+        assertEquals("the", sorted[2])
+    }
+
+}
