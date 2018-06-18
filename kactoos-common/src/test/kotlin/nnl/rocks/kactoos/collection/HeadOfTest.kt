7722f92d8ed5a5364c4097b5f3fc99d6652e86ba
Neonailol <neonailol@gmail.com>
2018-06-18 18:54:00 +0300

    More tests for HeadOf collection in common

--- kactoos-common/src/test/kotlin/nnl/rocks/kactoos/collection/HeadOfTest.kt
+++ kactoos-common/src/test/kotlin/nnl/rocks/kactoos/collection/HeadOfTest.kt
@@ -1,14 +1,31 @@
 package nnl.rocks.kactoos.collection
 
+import nnl.rocks.kactoos.iterable.IterableOf
 import nnl.rocks.kactoos.test.BehavesAsCollection
 import kotlin.test.Test
 
 class HeadOfTest {
 
     @Test
-    fun head() {
+    fun headOfCollection() {
         BehavesAsCollection(
             HeadOf(2, listOf(1, 2, 3)),
+            arrayOf(1, 2)
+        )
+    }
+
+    @Test
+    fun headOfVarargs() {
+        BehavesAsCollection(
+            HeadOf(2, 1, 2, 3),
+            arrayOf(1, 2)
+        )
+    }
+
+    @Test
+    fun headOfIterable() {
+        BehavesAsCollection(
+            HeadOf(2, IterableOf(1, 2, 3)),
             arrayOf(1, 2)
         )
     }
