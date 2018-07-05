876e967376ebaaeac31d216f18d35ea799be1f17
Neonailol <neonailol@gmail.com>
2018-07-05 20:11:41 +0300

    Remove duplicated code from IteratorOf

--- kactoos-common/src/main/kotlin/nnl/rocks/kactoos/iterator/IteratorOf.kt
+++ kactoos-common/src/main/kotlin/nnl/rocks/kactoos/iterator/IteratorOf.kt
@@ -1,27 +1,17 @@
 package nnl.rocks.kactoos.iterator
 
-import nnl.rocks.kactoos.internal.isEmpty
-
 /**
- * Iterator that returns the set of elements.
+ * Iterator over variable number of elements.
  *
  * There is no thread-safety guarantee.
  *
  * @param X Type of item
- * @param items Items to iterate
  * @since 0.3
  */
 class IteratorOf<out X : Any> private constructor(
-    private val items: Array<out X>,
-    private var position: Int
-) : Iterator<X> {
+    private val origin: Iterator<X>
+) : Iterator<X> by origin {
 
-    constructor(vararg items: X) : this(items, 0)
+    constructor(vararg items: X) : this(Immutable(items.iterator()))
 
-    override fun hasNext(): Boolean = position < items.size
-
-    override fun next(): X = when {
-        isEmpty() -> throw NoSuchElementException("The iterator doesn't have any more items")
-        else -> items[position ++]
-    }
 }
