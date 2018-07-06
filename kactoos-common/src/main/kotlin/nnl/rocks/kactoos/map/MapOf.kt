2c0afcb9cbbd99ca5b8911f3d6f495cd98c2b24d
Neonailol <neonailol@gmail.com>
2018-07-06 19:05:23 +0300

    Performance optimization for constructor like methods in MapOf

--- kactoos-common/src/main/kotlin/nnl/rocks/kactoos/map/MapOf.kt
+++ kactoos-common/src/main/kotlin/nnl/rocks/kactoos/map/MapOf.kt
@@ -13,60 +13,69 @@ import nnl.rocks.kactoos.iterable.Mapped
  *
  * @param X Type of key
  * @param Y Type of value
- * @see StickyMap
+ * @constructor Map from iterable [Map.Entry]
  * @since 0.4
  */
 class MapOf<X : Any, Y : Any>(
     entries: Iterable<Map.Entry<X, Y>>
 ) : MapEnvelope<X, Y>({ entries.map { entry: Map.Entry<X, Y> -> entry.key to entry.value }.toMap() }) {
 
-    constructor(vararg list: Map.Entry<X, Y>) : this(IterableOf(list.asIterable()))
+    /**
+     * @constructor Map from vararg [Map.Entry]
+     */
+    constructor(vararg entries: Map.Entry<X, Y>) : this(IterableOf(entries.asIterable()))
 
-    constructor(src: Map<X, Y>, vararg list: Map.Entry<X, Y>) : this(src, IterableOf(list.asIterable()))
+    /**
+     * @constructor Extend Map with vararg entries
+     */
+    constructor(source: Map<X, Y>, vararg entries: Map.Entry<X, Y>) : this(source, IterableOf(entries.asIterable()))
 
-    constructor(src: Map<X, Y>, list: Iterable<Map.Entry<X, Y>>) : this(Joined(src.entries, list))
+    /**
+     * @constructor Extend Map with iterable entries
+     */
+    constructor(source: Map<X, Y>, entries: Iterable<Map.Entry<X, Y>>) : this(Joined(source.entries, entries))
 
     companion object {
 
-        operator fun <X : Any, Y : Any, Z : Any> invoke(
-            key: KFunc<Z, X>,
-            value: KFunc<Z, Y>,
+        inline operator fun <X : Any, Y : Any, Z : Any> invoke(
+            crossinline key: KFunc<Z, X>,
+            crossinline value: KFunc<Z, Y>,
             list: Iterable<Z>
         ): MapOf<X, Y> {
             return MapOf(
-                { item -> MapEntry(key.invoke(item), value.invoke(item)) }, list
+                { item -> MapEntry(key(item), value(item)) }, list
             )
         }
 
-        operator fun <X : Any, Y : Any, Z : Any> invoke(
-            key: KFunc<Z, X>,
-            value: KFunc<Z, Y>,
+        inline operator fun <X : Any, Y : Any, Z : Any> invoke(
+            crossinline key: KFunc<Z, X>,
+            crossinline value: KFunc<Z, Y>,
             src: Map<X, Y>,
             list: Iterable<Z>
         ): MapOf<X, Y> {
             return MapOf(
-                { item -> MapEntry(key.invoke(item), value.invoke(item)) },
+                { item -> MapEntry(key(item), value(item)) },
                 src,
                 list
             )
         }
 
-        operator fun <X : Any, Y : Any, Z : Any> invoke(
-            entry: KFunc<Z, Map.Entry<X, Y>>,
+        inline operator fun <X : Any, Y : Any, Z : Any> invoke(
+            noinline entry: KFunc<Z, Map.Entry<X, Y>>,
             list: Iterable<Z>
         ): MapOf<X, Y> {
             return MapOf(Mapped(entry, list))
         }
 
-        operator fun <X : Any, Y : Any, Z : Any> invoke(
-            entry: KFunc<Z, Map.Entry<X, Y>>,
+        inline operator fun <X : Any, Y : Any, Z : Any> invoke(
+            noinline entry: KFunc<Z, Map.Entry<X, Y>>,
             src: Map<X, Y>,
             list: Iterable<Z>
         ): MapOf<X, Y> {
             return MapOf(src, Mapped(entry, list))
         }
 
-        operator fun <X : Any, Y : Any, Z : Any> invoke(
+        inline operator fun <X : Any, Y : Any, Z : Any> invoke(
             key: Func<Z, X>,
             value: Func<Z, Y>,
             list: Iterable<Z>
@@ -76,7 +85,7 @@ class MapOf<X : Any, Y : Any>(
             )
         }
 
-        operator fun <X : Any, Y : Any, Z : Any> invoke(
+        inline operator fun <X : Any, Y : Any, Z : Any> invoke(
             key: Func<Z, X>,
             value: Func<Z, Y>,
             src: Map<X, Y>,
@@ -89,14 +98,14 @@ class MapOf<X : Any, Y : Any>(
             )
         }
 
-        operator fun <X : Any, Y : Any, Z : Any> invoke(
+        inline operator fun <X : Any, Y : Any, Z : Any> invoke(
             entry: Func<Z, Map.Entry<X, Y>>,
             list: Iterable<Z>
         ): MapOf<X, Y> {
             return MapOf(Mapped(entry, list))
         }
 
-        operator fun <X : Any, Y : Any, Z : Any> invoke(
+        inline operator fun <X : Any, Y : Any, Z : Any> invoke(
             entry: Func<Z, Map.Entry<X, Y>>,
             src: Map<X, Y>,
             list: Iterable<Z>
