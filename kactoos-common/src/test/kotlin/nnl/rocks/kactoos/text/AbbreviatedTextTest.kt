136f5c2567efeb28116d77e57a18f449f3a65c49
Neonailol <neonailol@gmail.com>
2018-07-13 13:07:07 +0300

    More tests for AbbreviatedText

--- kactoos-common/src/test/kotlin/nnl/rocks/kactoos/text/AbbreviatedTextTest.kt
+++ kactoos-common/src/test/kotlin/nnl/rocks/kactoos/text/AbbreviatedTextTest.kt
@@ -6,11 +6,74 @@ import kotlin.test.Test
 class AbbreviatedTextTest {
 
     @Test
-    fun abbreviates() {
+    fun abbreviatesAnEmptyText() {
+        AssertTextsEquals(
+            AbbreviatedText("", 8),
+            TextOf { "" },
+            "Can't abbreviate empty string"
+        )
+    }
+
+    @Test
+    fun abbreviatesText() {
         AssertTextsEquals(
             AbbreviatedText("Hello, World!", 8),
             TextOf { "Hello..." },
-            "Can't abbreviate string"
+            "Can't abbreviate a string"
+        )
+    }
+
+    @Test
+    fun abbreviatesTextOneCharSmaller() {
+        AssertTextsEquals(
+            AbbreviatedText("oo programming", 10),
+            TextOf { "oo prog..." },
+            "Can't abbreviate a text one char smaller"
+        )
+    }
+
+    @Test
+    fun abbreviatesTextWithSameLength() {
+        AssertTextsEquals(
+            AbbreviatedText("elegant objects", 15),
+            TextOf { "elegant objects" },
+            "Can't abbreviate a text with same length"
+        )
+    }
+
+    @Test
+    fun abbreviatesTextOneCharBigger() {
+        AssertTextsEquals(
+            AbbreviatedText("the old mcdonald", 17),
+            TextOf { "the old mcdonald" },
+            "Can't abbreviate a text one char bigger"
+        )
+    }
+
+    @Test
+    fun abbreviatesTextTwoCharsBigger() {
+        AssertTextsEquals(
+            AbbreviatedText("hi everybody!", 15),
+            TextOf { "hi everybody!" },
+            "Can't abbreviate a text two chars bigger"
+        )
+    }
+
+    @Test
+    fun abbreviatesTextWithWidthBiggerThanLength() {
+        AssertTextsEquals(
+            AbbreviatedText("cactoos framework", 50),
+            TextOf { "cactoos framework" },
+            "Can't abbreviate a text with width bigger than length"
+        )
+    }
+
+    @Test
+    fun abbreviatesTextBiggerThanDefaultMaxWidth() {
+        AssertTextsEquals(
+            AbbreviatedText("The quick brown fox jumps over the lazy black dog and after that returned to the cave"),
+            TextOf { "The quick brown fox jumps over the lazy black dog and after that returned to ..." },
+            "Can't abbreviate a text bigger than default max width"
         )
     }
 }
