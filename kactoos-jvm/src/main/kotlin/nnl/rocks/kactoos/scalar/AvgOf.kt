b6ced23660e6eab18ffd7b6128574656165271d7
Neonailol <neonailol@gmail.com>
2018-08-10 12:43:40 +0300

    Remove unnecessary map operation in AvgOf

--- kactoos-jvm/src/main/kotlin/nnl/rocks/kactoos/scalar/AvgOf.kt
+++ kactoos-jvm/src/main/kotlin/nnl/rocks/kactoos/scalar/AvgOf.kt
@@ -3,6 +3,7 @@ package nnl.rocks.kactoos.scalar
 import nnl.rocks.kactoos.KScalar
 import nnl.rocks.kactoos.Scalar
 import nnl.rocks.kactoos.func.FuncOf
+import nnl.rocks.kactoos.iterable.IterableOf
 import nnl.rocks.kactoos.iterable.LengthOf
 import nnl.rocks.kactoos.iterable.Mapped
 import java.math.BigDecimal
@@ -26,49 +27,75 @@ class AvgOf private constructor(value: KScalar<Double>) : NumberEnvelope(value) 
 
     private constructor(value: Scalar<Double>) : this({ value() })
 
-    constructor(src: Iterable<KScalar<Number>>) : this(
+    constructor(
+        src: Iterable<KScalar<Number>>
+    ) : this(
         Ternary(
             LengthOf(src).toLong(),
             FuncOf { len -> len > 0 },
             FuncOf { len ->
-                src.map { BigDecimal.valueOf(it().toDouble()) }
-                    .fold(BigDecimal.ZERO) { acc, r -> acc.add(r, MathContext.DECIMAL128) }
-                    .divide(BigDecimal.valueOf(len), MathContext.DECIMAL128)
+                src
+                    .fold(
+                        BigDecimal.ZERO
+                    ) { accumulator, element ->
+                        accumulator.add(BigDecimal.valueOf(element().toDouble()), MathContext.UNLIMITED)
+                    }
+                    .divide(BigDecimal.valueOf(len), MathContext.UNLIMITED)
                     .toDouble()
             },
             FuncOf { 0.0 }
         )
     )
 
-    constructor(vararg src: Int) : this(
+    constructor(
+        vararg src: Int
+    ) : this(
         Mapped(
             { number: Int -> { number } },
             src.asIterable()
         )
     )
 
-    constructor(vararg src: Long) : this(
+    constructor(
+        vararg src: Long
+    ) : this(
         Mapped(
             { number: Long -> { number } },
             src.asIterable()
         )
     )
 
-    constructor(vararg src: Double) : this(
+    constructor(
+        vararg src: Double
+    ) : this(
         Mapped(
             { number: Double -> { number } },
             src.asIterable()
         )
     )
 
-    constructor(vararg src: Float) : this(
+    constructor(
+        vararg src: Float
+    ) : this(
         Mapped(
             { number: Float -> { number } },
             src.asIterable()
         )
     )
 
-    constructor(vararg src: KScalar<Number>) : this(Iterable { src.iterator() })
+    constructor(
+        vararg src: KScalar<Number>
+    ) : this(
+        IterableOf(src.asIterable())
+    )
 
-    constructor(vararg src: Scalar<Number>) : this(Iterable { src.map { { it() } }.iterator() })
+    constructor(
+        vararg src: Scalar<Number>
+    ) : this(
+        Mapped(
+            { number: Scalar<Number> -> { number() } },
+            src.asIterable()
+        )
+    )
+
 }
